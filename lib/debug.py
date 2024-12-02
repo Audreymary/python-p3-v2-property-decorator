92a85a2bae80b9781d04661fbf8fa891c2c18cf3
Audreymary <audreymoraa006@gmail.com>
2024-12-02 14:27:08 +0300

    add initial commit

--- lib/debug.py
+++ lib/debug.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 
-from dog import Dog
-import ipdb
+# from dog import Dog
+# import ipdb
 
-ipdb.set_trace()
+# ipdb.set_trace()
+
+
+from dog import Dog
+import pytest
+
+class TestDogProperties:
+    def test_name_breed_valid(self):
+        try:
+            dog = Dog("Fido", "Corgi")
+        except Exception as exc:
+            assert False, f'Dog("Fido", "Corgi") raised an exception {exc}'
+
+    def test_name_is_string_valid_length(self):
+        dog = Dog("Fido", "Corgi")
+        with pytest.raises(ValueError):
+            dog.name = 7  # not a string
+        with pytest.raises(ValueError):
+            dog.name = ''  # too short
+        with pytest.raises(ValueError):
+            dog.name = 'Fido the adorable Corgi who likes to steal socks'  # too long
+
+    def test_breed_is_approved_breed(self):
+        dog = Dog("Snoopy", "Beagle")
+        with pytest.raises(ValueError):
+            dog.breed = "Poodle"  # not an approved breed
